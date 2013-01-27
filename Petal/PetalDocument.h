9a312c92a19ebdab14c9b1739f81496addcf971c
Sean Moore <sean.robert.moore@me.com>
2013-01-27 11:44:47 -0500

    start tableview cell design

--- Petal/PetalDocument.h
+++ Petal/PetalDocument.h
@@ -2,12 +2,19 @@
 //  PetalDocument.h
 //  Petal
 //
-//  Created by Sean Moore on 11/10/12.
-//  Copyright (c) 2012 Sean Moore. All rights reserved.
+//  Created by Sean Moore on 1/27/13.
+//  Copyright (c) 2013 Sean Moore. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface PetalDocument : UIDocument
+
+@property (nonatomic) NSString * content;
+@property (nonatomic) NSString * title;
+@property (nonatomic, readonly) NSString * previewText;
+@property (nonatomic, readonly) NSDate * createdAt;
+@property (nonatomic, readonly) NSDate * lastModified;
+@property (nonatomic) NSArray * tags;
 
 @end
--- Petal/PetalDocument.m
+++ Petal/PetalDocument.m
@@ -2,12 +2,44 @@
 //  PetalDocument.m
 //  Petal
 //
-//  Created by Sean Moore on 11/10/12.
-//  Copyright (c) 2012 Sean Moore. All rights reserved.
+//  Created by Sean Moore on 1/27/13.
+//  Copyright (c) 2013 Sean Moore. All rights reserved.
 //
 
 #import "PetalDocument.h"
 
+
+
 @implementation PetalDocument
+
+- (void)setContent:(NSString *)content {
+    
+    
+}
+
+- (NSString *)title {
+    
+    return @"Petal Document Title";
+}
+
+- (NSString *)previewText {
+    
+    return @"This is a preview for the petal document. Lorem ipsum. Delore sit amet.";
+}
+
+- (NSDate *)lastModified {
+    
+    return [NSDate date];
+}
+
+- (NSDate *)createdAt {
+    
+    return [NSDate dateWithTimeIntervalSinceNow:-10000];
+}
+
+- (NSArray *)tags {
+    
+    return @[@"hello", @"world", @"tags"];
+}
 
 @end
